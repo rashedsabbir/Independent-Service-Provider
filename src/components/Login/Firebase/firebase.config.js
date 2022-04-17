abb25ee19cc451b0bb50621394a5a544b9a4e1f2
rashedsabbir <rashedulhasan1996@gmail.com>
2022-04-18 03:31:22 +0600

    api-key-added

--- src/components/Login/Firebase/firebase.config.js
+++ src/components/Login/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCQEzucHBhYOrw6Obl_p_d2ifwlW327GSU",
-    authDomain: "travelo-tour.firebaseapp.com",
-    projectId: "travelo-tour",
-    storageBucket: "travelo-tour.appspot.com",
-    messagingSenderId: "518910032164",
-    appId: "1:518910032164:web:622caff45b8e4a7b735bb1"
-  };
+  apiKey: "AIzaSyCOku_EYXJCM4vPRMzuox5y989cDmGuM80",
+  authDomain: "assignment-10-8dff4.firebaseapp.com",
+  projectId: "assignment-10-8dff4",
+  storageBucket: "assignment-10-8dff4.appspot.com",
+  messagingSenderId: "669153175240",
+  appId: "1:669153175240:web:70456b4e8a64bde284e7fd"
+};
 
 export default firebaseConfig;
