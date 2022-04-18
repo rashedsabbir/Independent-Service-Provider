d0490d6f252db65fc6f3b46d545ea50e0001c938
rashedsabbir <rashedulhasan1996@gmail.com>
2022-04-18 21:35:35 +0600

    logo changed

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,53 +1,46 @@
-import { faEnvelope, faMapMarkerAlt, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import React from 'react';
+import { Accordion } from 'react-bootstrap';
+
 import './Contact.css';
-import contactImg from '../../img/contact-us.svg'
+
 
 const Contact = () => {
-   // contact data
+
+   //////////////  Blog  /////////////////
 
    return (
       <>
          <div className="contact_banner">
-            <div data-aos="fade-up"><h3>Contact</h3></div>
+            <div data-aos="fade-up"><h3>Blogs</h3></div>
          </div>
          <div className="contact_wrapper">
             <div className="container">
                <div className="row my-5" data-aos="fade-up">
+                  <Accordion defaultActiveKey="0">
+                     <Accordion.Item eventKey="0">
+                        <Accordion.Header>Do your tours include international airfare?</Accordion.Header>
+                        <Accordion.Body>
+                           Travelers arrive from all over the world to begin our tours and it is not possible to include international airfare in our prices. We would be happy to help you arrange flights. Just ask for an air quote when making your booking.
+                        </Accordion.Body>
+                     </Accordion.Item>
+                     <Accordion.Item eventKey="1">
+                        <Accordion.Header>Can you help arrange my travel visas?</Accordion.Header>
+                        <Accordion.Body>
+                           Requirements for travel visas vary widely depending on your nationality and your destination. Although we are unable to arrange visas on your behalf we have provided links on our “Resources” page to websites that will help you determine if visas are required.
+                        </Accordion.Body>
+                     </Accordion.Item>
+                     <Accordion.Item eventKey="2">
+                        <Accordion.Header>How many people can join a tour?</Accordion.Header>
+                        <Accordion.Body>
+                           We keep our group sizes low so you have the freedom to move around and get involved with your surroundings, as well as more personal attention from our local guides. This intimate size ensures that your group will not crowd your experience.
+                        </Accordion.Body>
+                     </Accordion.Item>
+                  </Accordion>
+               </div>
 
-               </div>
-               <div className="row mt-5 align-items-center" data-aos="fade-up">
-                  <div className="col-lg-6">
-                     <div className="contact-img">
-                        <img src={contactImg} alt="" />
-                     </div>
-                  </div>
-                  <div className="col-lg-6">
-                     <div className="contact-form">
-                        <form>
-                           <div className="row">
-                              <div className="col-lg-6">
-                                 <input type="text" placeholder="Name" />
-                              </div>
-                              <div className="col-lg-6">
-                                 <input type="email" placeholder="Email" />
-                              </div>
-                           </div>
-                           <div className="row">
-                              <div className="col-lg-6">
-                                 <input type="number" placeholder="Phone Number" />
-                              </div>
-                              <div className="col-lg-6">
-                                 <input type="text" placeholder="Subject" />
-                              </div>
-                           </div>
-                           <textarea cols="20" rows="7" placeholder="Write message"></textarea>
-                           <input className="mt-3" type="submit" value="Submit" />
-                        </form>
-                     </div>
-                  </div>
-               </div>
+
+
             </div>
          </div>
       </>
